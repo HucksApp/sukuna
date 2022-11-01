26e8d4dd6deb1a021b20b8603a78d93455629b32
Jason Hucks <jasonhucks@Jasons-MacBook-Air.local>
2022-11-01 02:47:16 -0700

    1st Commit

---
+++ crypto_spam/test.py
@@ -0,0 +1,15 @@
+from cryptic import start_brute_force
+
+
+ITERATION_NUMBER=3000      # Any amount
+wallet_std=12         # Wallet strenght ! 12 15 18 21 24
+
+
+
+
+
+
+if __name__ == '__main__': 
+    start_brute_force(ITERATION_NUMBER,wallet_std)
+
+
